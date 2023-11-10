b1f970da5b342ffdd97e0d6e1d06a3bcc04f89d9
Lks0411 <128689605+Lks0411@users.noreply.github.com>
2023-11-10 16:06:41 +0800

    Update lab1 q1 (a).cpp

--- lab1/lab1 q1 (a).cpp
+++ lab1/lab1 q1 (a).cpp
@@ -10,6 +10,8 @@ int main()
     cout << setw(10) << "Artist" << setw(10) << "title" << setw(10) << "price" << setw(10) << "Genre" << setw(10) << "Disc" << setw(10) << "Sale" << endl;
     cout << setw(10) << "Merle" << setw(10) << "Blue" << setw(10) << "12.99" << setw(10) << "Country" << setw(10) << "4%" << setw(10) << "12.47" << endl;
     cout << setw(10) << "Richard" << setw(10) << "Music" << setw(10) << "8.49" << setw(10) << "Classical" << setw(10) << "8%" << setw(10) << "7.81" << endl;
+    cout << "Hello world";
+    
     return 0;
 }
 
